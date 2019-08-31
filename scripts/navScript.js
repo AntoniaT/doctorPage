5f02f7c1c227175daddc90c9117087608cd3f3cd
Antonia <gintoni@gmx.at>
2019-08-31 08:29:29 +0200

    set up html and basic sass for layout

---
+++ scripts/navScript.js
@@ -0,0 +1,9 @@
+'use strict'
+
+function openNav() {
+    document.getElementById("mobile-nav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mobile-nav").style.width = "0";
+  }
