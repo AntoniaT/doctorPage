7c247e60594fad8720e0ec499b3c8a2028416750
Antonia <gintoni@gmx.at>
2019-09-30 21:18:56 +0200

    styling and adding content

---
+++ Texte.js
@@ -0,0 +1,7 @@
+const leistungen = [
+    {
+        id: 1,
+        title : 'Gastroskopie (Magenspiegelung)',
+        copy: 'Oberbauchbeschwerden sind keine Seltenheit. Speiseröhre, Magen und Zwölffingerdarm werden von innen, mit Hilfe eines flexiblen Endoskops, optisch untersucht. Durch diese Untersuchung wird eine Beurteilung von Veränderungen wie zum Beispiel Entzündungen, Polypen (gutartige Wucherungen der Schleimhaut) oder Geschwüre ermöglicht, um frühzeitig die richtige Diagnose und Therapie zu finden.  Je nach Notwendigkeit werden im Zuge der Magenspiegelung Gewebeproben entnommen, welche zur weiteren histologischen Untersuchung eingesandt werden. Sollte eine Polypen-Entfernung (Polypektomie) notwendig werden, so kann dies ebenfalls im Rahmen der Untersuchung durchgeführt werden.'
+    }
+]
