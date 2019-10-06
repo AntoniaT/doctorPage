735cb8c60f6e3ca7b3173e135f9adf36d5921d2b
Antonia <gintoni@gmx.at>
2019-10-06 20:10:49 +0200

    add more styling and images

--- scripts/index.js
+++ scripts/index.js
@@ -17,3 +17,15 @@ const openLeistung = () =>{
     window.location.assign('leistungen.html');
 }
 document.querySelector('.leistungenBtn').addEventListener('click', openLeistung);
+
+// ON SCROLL NAVBAR
+let navBar = document.querySelector('.main-nav');
+
+window.onscroll = () => {
+    let scrollPos = window.pageYOffset;
+    if(scrollPos > 10) {
+       navBar.classList.add('white');
+    } else if(scrollPos <= 100) {
+       navBar.classList.remove('white');
+  }
+}
